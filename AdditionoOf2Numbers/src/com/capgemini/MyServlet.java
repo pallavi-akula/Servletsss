f9f42c7cee69f0f013723595827acd07aa0baf78
pallavi-akula <43680336+pallavi-akula@users.noreply.github.com>
2018-12-28 14:58:53 +0530

    Add files via upload

---
+++ AdditionoOf2Numbers/src/com/capgemini/MyServlet.java
@@ -0,0 +1,56 @@
+package com.capgemini;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	try(PrintWriter out = response.getWriter()){
+		int x,y;
+		int sum;
+	
+   String a= request.getParameter("num1");
+   String b= request.getParameter("num2");
+   x=Integer.parseInt(a);
+   y=Integer.parseInt(b);
+   sum =x+y;
+ 
+	out.println("Addition of two numbers"+sum);
+	}
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
